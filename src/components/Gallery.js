c16617494a5203ad5a10e5c568cbbffd84ee7383
Wish Marukapitak <97574512+wish-eq@users.noreply.github.com>
2023-11-10 01:38:51 +0700

    fix grid for all components

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -10,24 +10,26 @@ function Gallery() {
       .catch((err) => console.log(err));
   }, []);
 
-  const allowedIds = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const allowedIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   const filteredRecords = records.filter((record) =>
     allowedIds.includes(record.id)
   );
   return (
     <div class="gallery">
-      <div class="photo-header">
+      <div class="photo-header-container">
         <p>Photos</p>
       </div>
-      <div class="photo-gallery">
-        <ul class="photo-list">
-          {filteredRecords.map((record) => (
-            <li key={record.id}>
-              <img class="photo" src={record.url} alt="Cheetah!" />
-            </li>
-          ))}
-        </ul>
+      <div class="photo-gallery-container">
+        <div class="photo-list-container">
+          <ul class="photo-list">
+            {filteredRecords.map((record) => (
+              <li class="photo-container" key={record.id}>
+                <img class="photo" src={record.url} alt="Cheetah!" />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
