111bf0cbdb3e45d8e0fba3d30a331715b900c0b7
Wish Marukapitak <97574512+wish-eq@users.noreply.github.com>
2023-11-11 02:23:25 +0700

    added icons and padding to side-bar

--- src/components/LeftBar.js
+++ src/components/LeftBar.js
@@ -1,4 +1,14 @@
 import React, { Component } from "react";
+import {
+  FaLocationArrow,
+  FaImage,
+  FaImages,
+  FaFolder,
+  FaMemory,
+  FaUserFriends,
+  FaGoogleDrive,
+  FaAlignJustify,
+} from "react-icons/fa";
 import profile from "../image/profile.png";
 
 export class Leftbar extends Component {
@@ -12,30 +22,57 @@ export class Leftbar extends Component {
         </div>
         <div class="library-container">
           <ul class="option-list">
-            <p class="list-header">Library</p>
-            <li>Photos</li>
-            <li>Albums</li>
-            <li>Folders</li>
+            <p class="menu-header menu-line">Library</p>
+            <li class="menu-line">
+              <FaImage class="icon" />
+              Photos
+            </li>
+            <li class="menu-line">
+              <FaImages class="icon" />
+              Albums
+            </li>
+            <li class="menu-line">
+              <FaFolder class="icon" />
+              Folders
+            </li>
           </ul>
         </div>
         <div class="collections-container">
           <ul class="option-list">
-            <p class="list-header">Collections</p>
-            <li>Memories</li>
-            <li>People</li>
-            <li>Locations</li>
+            <p class="menu-header menu-line">Collections</p>
+            <li class="menu-line">
+              <FaMemory class="icon" />
+              Memories
+            </li>
+            <li class="menu-line">
+              <FaUserFriends class="icon" />
+              People
+            </li>
+            <li class="menu-line">
+              <FaLocationArrow class="icon" />
+              Locations
+            </li>
           </ul>
         </div>
         <div class="storage-container">
           <ul class="option-list">
-            <p class="list-header">Storage</p>
-            <li>OneDrive</li>
-            <li>Google Drive</li>
+            <p class="menu-header menu-line">Storage</p>
+            <li class="menu-line">
+              <FaGoogleDrive class="icon" />
+              OneDrive
+            </li>
+            <li class="menu-line">
+              <FaGoogleDrive class="icon" />
+              Google Drive
+            </li>
           </ul>
         </div>
         <div class="setting-container">
           <ul class="option-list">
-            <p class="setting">Settings</p>
+            <p class="setting menu-line">
+              <FaAlignJustify class="icon" />
+              Settings
+            </p>
           </ul>
         </div>
       </div>
