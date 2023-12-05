f3904513d57f7e28aee7014b5d5aa9a4082b9e2c
Wish Marukapitak <97574512+wish-eq@users.noreply.github.com>
2023-12-05 17:45:37 +0700

    added setting modal

--- src/components/leftBar/SettingBox.js
+++ src/components/leftBar/SettingBox.js
@@ -1,18 +1,36 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { FaAlignJustify } from "react-icons/fa";
 
-export class SettingBox extends Component {
-  render() {
-    return (
-      <div class="setting-container">
-        <ul class="option-list">
-          <p class="setting menu-line">
-            <FaAlignJustify class="icon" />
-            Settings
-          </p>
-        </ul>
-      </div>
-    );
-  }
+export default function SettingBox() {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  return (
+    <div class="setting-container">
+      {/* <button onClick={toggleModal} className="btn-modal">
+        OPEN
+      </button> */}
+      <ul class="option-list">
+        <p class="setting menu-line" onClick={toggleModal}>
+          <FaAlignJustify class="icon" />
+          Settings
+        </p>
+      </ul>
+      {modal && (
+        <div className="modal">
+          <div className="overlay"></div>
+          <div className="modal-content">
+            <p>Setting</p>
+
+            <button className="close-modal" onClick={toggleModal}>
+              CLOSE
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
-export default SettingBox;
