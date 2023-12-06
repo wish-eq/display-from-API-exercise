286cf1ac217d003d86d7222b53866fd932787d8a
Wish Marukapitak <97574512+wish-eq@users.noreply.github.com>
2023-12-07 00:28:36 +0700

    changed GalleryContainer.js template

--- src/components/leftBar/ProfileBox.js
+++ src/components/leftBar/ProfileBox.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
 import profile from "../../image/profile.png";
 
-export class ProfileBox extends Component {
-  render() {
-    return (
-      <div className="profile-container flex flex-col items-center justify-center">
-        <img id="profile-img" src={profile} alt="profile" />
-        <p class="name">Wish</p>
-        <p class="email">wish.eq@gmail.com</p>
-      </div>
-    );
-  }
+export default function ProfileBox() {
+  return (
+    <div className="profile-container flex flex-col items-center justify-center">
+      <img id="profile-img" src={profile} alt="profile" />
+      <p class="name">Wish</p>
+      <p class="email">wish.eq@gmail.com</p>
+    </div>
+  );
 }
-export default ProfileBox;
