bb5c24013453dd614a6aa1b00ce8df9b95257b06
Wish Marukapitak <97574512+wish-eq@users.noreply.github.com>
2023-12-17 22:06:17 +0700

    changed picture shown

--- src/components/galleryPage/GalleryContainer.js
+++ src/components/galleryPage/GalleryContainer.js
@@ -11,7 +11,7 @@ export default function GalleryContainer() {
       .catch((err) => console.log(err));
   }, []);
 
-  const allowedIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 25];
+  const allowedIds = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 26];
 
   const filteredRecords = records.filter((record) =>
     allowedIds.includes(record.id)
