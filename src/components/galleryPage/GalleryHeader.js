5bfaf9e3929390c3823bbfbb3e3a637ba9106d61
Wish Marukapitak <97574512+wish-eq@users.noreply.github.com>
2023-12-18 20:06:10 +0700

    added author modal

--- src/components/galleryPage/GalleryHeader.js
+++ src/components/galleryPage/GalleryHeader.js
@@ -1,20 +1,46 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { FaAddressBook } from "react-icons/fa";
 
-export class GalleryHeader extends Component {
-  render() {
-    return (
-      <div class="photo-header-container">
-        <div class="photo-header-header flex justify-between">
-          <div>
-            <p class="photo-header">Photos</p>
-          </div>
-          <div class="header-icon-container">
-            <FaAddressBook class="header-icon" size={50} />
-          </div>
+export default function GalleryHeader() {
+  const [authorModal, setAuthorModal] = useState(false);
+
+  const toggleAuthorModal = () => {
+    setAuthorModal(!authorModal);
+  };
+
+  return (
+    <div class="photo-header-container">
+      <div class="photo-header-header flex justify-between">
+        <div>
+          <p class="photo-header">Photos</p>
+        </div>
+        <div class="header-icon-container">
+          <FaAddressBook
+            class="header-icon"
+            size={50}
+            onClick={toggleAuthorModal}
+          />
         </div>
       </div>
-    );
-  }
+      {authorModal && (
+        <div className="modal">
+          <div className="overlay" onClick={toggleAuthorModal}>
+            <div className="author-modal-content flex flex-col  items-center justify-between ">
+              <p className="text-xl font-semibold">Author</p>
+              <div className="flex flex-col items-center justify-between ">
+                <p>👋 Hi! I'm Wish Marukapitak</p>
+                <p>you can see me more at</p>
+                <a href="https://github.com/wish-eq">
+                  https://github.com/wish-eq
+                </a>
+              </div>
+              <button className="close-modal" onClick={toggleAuthorModal}>
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
-export default GalleryHeader;
